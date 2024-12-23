0538ba461cffb7958a4ddb83c083586baec7696c
Sukjoon Hong <sj.hong@anypointmedia.com>
2024-12-23 16:20:49 +0900

    first commit

---
+++ src/main/kotlin/com/bogu/bogubackend/BoguBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.bogu.bogubackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoguBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoguBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/bogu/bogubackend/BoguBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bogu.bogubackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoguBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
